2f9b45ff2e0a5440dc85ca9efcada131948ec67d
huilinqiu <187346709@qq.com>
2017-12-25 15:00:23 +0800

    1225002

--- mobile/js/swiper-param.js
+++ mobile/js/swiper-param.js
@@ -1,5 +1,5 @@
 (function(){
-	// swiper 3.4.1 初始化：
+// swiper 3.4.1 初始化：
 	var mySwiper = new Swiper('.swiper-container', {
 		direction: 'horizontal',
 		loop: true,
@@ -8,20 +8,17 @@
 		paginationClickable: true,
 		pagination: '.swiper-pagination',
 		paginationType: 'custom',
-	
+		paginationCustomRender: function (swiper, current, total) {
+            var _html = '';
+            for (var i = 1; i <= total; i++) {
+              if (current == i) {
+                _html += '<span class="pagination-item pagination-item-active"></span>';
+              }else{
+                _html += '<span class="pagination-item"></span>';
+              }
+            }
+            return _html;//返回所有的页码html
+        }
 	});
 	
-	// 控制分页active变化  
-	//var items = document.querySelectorAll('.pagination-item');
-	//if(items) {
-	//	setInterval(function() {
-	//		var current = 4 - mySwiper.activeIndex;
-	//		for(var i = 0; i < items.length; i++) {
-	//			if(items[i].classList.contains('pagination-item-active')) {
-	//				items[i].classList.remove('pagination-item-active');
-	//			}
-	//		}
-	//		items[current].classList.add('pagination-item-active');
-	//	}, 1500);
-	//}		
 })();
