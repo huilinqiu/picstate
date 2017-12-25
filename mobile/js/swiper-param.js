05dffa54a4da68a346d72d5c9799afa3f41991c3
huilinqiu <187346709@qq.com>
2017-12-25 09:42:01 +0800

    122502

--- mobile/js/swiper-param.js
+++ mobile/js/swiper-param.js
@@ -11,15 +11,15 @@ var mySwiper = new Swiper('.swiper-container', {
 });
 
 // 控制分页active变化  
-var items = document.querySelectorAll('.pagination-item');
-if(items) {
-	setInterval(function() {
-		var current = 4 - mySwiper.activeIndex;
-		for(var i = 0; i < items.length; i++) {
-			if(items[i].classList.contains('pagination-item-active')) {
-				items[i].classList.remove('pagination-item-active');
-			}
-		}
-		items[current].classList.add('pagination-item-active');
-	}, 1500);
-}
+//var items = document.querySelectorAll('.pagination-item');
+//if(items) {
+//	setInterval(function() {
+//		var current = 4 - mySwiper.activeIndex;
+//		for(var i = 0; i < items.length; i++) {
+//			if(items[i].classList.contains('pagination-item-active')) {
+//				items[i].classList.remove('pagination-item-active');
+//			}
+//		}
+//		items[current].classList.add('pagination-item-active');
+//	}, 1500);
+//}
